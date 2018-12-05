6aaad77b0f51fa942eb93043c8052e98f807d3d0
lalitaSa <42977268+lalitaSa@users.noreply.github.com>
2018-12-05 11:14:29 +0700

    Update graph file.

--- Pygame_RPG/Graph.py
+++ Pygame_RPG/Graph.py
@@ -1,26 +1,17 @@
-""" Graph generator """
-print("Loading resource.." ,end='..')
+# -*- coding: utf-8 -*-
+print("กำลังโหลดทรัพยากร.." ,end='..')
 
 import plotly
 from plotly.graph_objs import Scatter, Layout
-import plotly.graph_objs as go
-
-#plotly.tools.set_credentials_file(username='it61070268', api_key='yt9KMI4UYOM6vNlH4Jvc')
-
-x_axis = [1,2,3,4]
-y_axis = [4,3,2,1]
-
-
-data = [go.Scatter(x=x_axis, y=y_axis)]
-layout = go.Layout(title="hello world")
-
-print("Completed!")
-print("Generating graph..", end="..")
-
-plotly.offline.plot({"data": data,"layout": layout}, auto_open=True)
-
-##plotly.offline.plot({"data": [Scatter(x=[1, 2, 3, 4], y=[4, 1, 3, 500])],
-##                          "layout": Layout(title="Score")}
-##                          )
-
-print("Completed!")
+print("สำเร็จ!")
+print("กำลังสร้างแผนภูมิ..", end="..")
+##print(plotly.__version__)  # version >1.9.4 required
+plotly.offline.plot({
+"data": [
+    Scatter(x=[1, 2, 3, 4], y=[4, 1, 3, 500])
+],
+"layout": Layout(
+    title="hello world"
+)
+})
+print("สำเร็จ!")
