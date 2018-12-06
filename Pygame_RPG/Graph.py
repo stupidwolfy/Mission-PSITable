a0261d129070389acb6f76722156ff3aaa3b3d1c
lalitaSa <42977268+lalitaSa@users.noreply.github.com>
2018-12-06 19:08:34 +0700

    Update graph file.

--- Pygame_RPG/Graph.py
+++ Pygame_RPG/Graph.py
@@ -1,17 +1,71 @@
-# -*- coding: utf-8 -*-
-print("กำลังโหลดทรัพยากร.." ,end='..')
+""" Graph generator """
+print("..:::Pygame RPG Graph generator:::..")
+print("Loading resource.." ,end='..')
 
 import plotly
-from plotly.graph_objs import Scatter, Layout
-print("สำเร็จ!")
-print("กำลังสร้างแผนภูมิ..", end="..")
-##print(plotly.__version__)  # version >1.9.4 required
-plotly.offline.plot({
-"data": [
-    Scatter(x=[1, 2, 3, 4], y=[4, 1, 3, 500])
-],
-"layout": Layout(
-    title="hello world"
+from plotly import tools
+import plotly.plotly as py
+import plotly.graph_objs as go
+from firebase import firebase
+
+url = 'https://pygame-rpg01-223910.firebaseio.com/'
+messenger = firebase.FirebaseApplication(url)
+
+fire_score = messenger.get('/Score', None) ## Get score in dict type
+fire_wave = messenger.get('/Wave', None) ## Get wave in dict type
+fire_time = messenger.get('/Time', None) ## Get time in dict type
+
+print("Done!")
+print("Generating graph....")
+
+fire_x, fire_y = list(), list()
+for s in fire_score:
+    fire_x.append('^'+str(s)+'^')
+    fire_y.append(fire_score[s])
+#Scatter
+Score = go.Bar(
+    x=fire_x,
+    y=fire_y,
+    name = 'Score',
+#    text=['Text A', 'Text B', 'Text C'],
 )
-})
-print("สำเร็จ!")
+
+fire_x, fire_y = list(), list()
+for w in fire_wave:
+    fire_x.append('^'+str(w)+'^')
+    fire_y.append(fire_wave[w])
+
+Wave = go.Bar(
+    x=fire_x,
+    y=fire_y,
+    name = 'Wave',
+#    text=['Text D', 'Text E', 'Text F'],
+#    textposition='inside'
+)
+
+fire_x, fire_y = list(), list()
+for t in fire_time:
+    fire_x.append('^'+str(t)+'^')
+    fire_y.append(fire_time[t])
+
+Timess = go.Bar(
+    x=fire_x,
+    y=fire_y,
+    name = 'Time',
+#    text=['Text D', 'Text E', 'Text F'],
+#    textposition='inside'
+)
+
+title = ('Score', 'Wave', 'Time', 'Total')
+fig = tools.make_subplots(rows=4, cols=1, subplot_titles=title )
+
+fig.append_trace(Score, 1, 1)
+fig.append_trace(Wave, 2, 1)
+fig.append_trace(Timess, 3, 1)
+fig.append_trace(Score, 4, 1)
+fig.append_trace(Wave, 4, 1)
+
+fig['layout'].update(height=1600, width=800, showlegend=False, title='Player Result')
+plotly.offline.plot(fig, filename='Graphv3.html')
+
+print("Completed!")
